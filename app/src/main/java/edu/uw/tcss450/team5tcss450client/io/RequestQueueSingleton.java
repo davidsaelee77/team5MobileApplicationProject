21d56561ae504aff2d538ce11b09b19fdcfb78e4
Gordon Tran <51973793+trangordon@users.noreply.github.com>
2020-05-05 17:46:12 -0700

    RequestQueueSingleton & UserInfoViewModel javadocs

--- app/src/main/java/edu/uw/tcss450/team5tcss450client/io/RequestQueueSingleton.java
+++ app/src/main/java/edu/uw/tcss450/team5tcss450client/io/RequestQueueSingleton.java
@@ -17,6 +17,12 @@ public class RequestQueueSingleton {
     private RequestQueue mRequestQueue;
     private ImageLoader mImageLoader;
 
+    /**
+     *Constructor that instantiates fields from given context.
+     * @author David Salee
+     * @version May 2020
+     * @param context
+     */
     private RequestQueueSingleton(Context context) {
         RequestQueueSingleton.context = context;
         mRequestQueue = getmRequestQueue();
@@ -38,6 +44,13 @@ public class RequestQueueSingleton {
                 });
     }
 
+    /**
+     * Gets the instance of the RequestQueueSingleton from context. Creates one if null.
+     * @author David Salee
+     * @version May 2020
+     * @param context
+     * @return instance
+     */
     public static synchronized RequestQueueSingleton getInstance(Context context) {
         if (instance == null) {
             instance = new RequestQueueSingleton(context);
@@ -45,6 +58,12 @@ public class RequestQueueSingleton {
         return instance;
     }
 
+    /**
+     *Returns mRequestQueue. Creates a new one if current is null.
+     * @author David Salee
+     * @version May 2020
+     * @return mRequestQueue
+     */
     public RequestQueue getmRequestQueue() {
         if (mRequestQueue == null) {
             // getApplicationContext() is key, it keeps you from leaking the
@@ -54,10 +73,23 @@ public class RequestQueueSingleton {
         return mRequestQueue;
     }
 
+    /**
+     *Adds request to current RequestQueue.
+     * @author David Salee
+     * @version May 2020
+     * @param req
+     * @param <T>
+     */
     public <T> void addToRequestQueue(Request<T> req) {
         getmRequestQueue().add(req);
     }
 
+    /**
+     *Returns mImageLoader.
+     * @author David Salee
+     * @version May 2020
+     * @return mImageLoader
+     */
     public ImageLoader getmImageLoader() {
         return mImageLoader;
     }
